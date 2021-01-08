ec42a8698f8b55db14984f7b44186f05e46ea1b1
matucker <matucker@adaptivehandyapps.com>
2021-01-08 11:54:16 -0500

    added presense sensor: off-wrist "--", permission denied: "XX"

--- HAM-fitbit-ondevice-app/app/index.js
+++ HAM-fitbit-ondevice-app/app/index.js
@@ -6,6 +6,7 @@ import * as util from "../common/utils";
 import { HeartRateSensor } from "heart-rate";
 import { display } from "display";
 import { me as appbit } from "appbit";
+import { BodyPresenceSensor } from "body-presence";
 
 // Update the clock every minute
 clock.granularity = "minutes";
@@ -13,6 +14,8 @@ clock.granularity = "minutes";
 // Get a handle on the <text> element
 const currentTimeLabel = document.getElementById("currentTimeLabel");
 const heartRateLabel = document.getElementById("heartRateLabel");
+
+const hrm = new HeartRateSensor({ frequency: 1 });
 
 // Update the currentTime <text> element every tick with the current time
 clock.ontick = (evt) => {
@@ -29,11 +32,24 @@ clock.ontick = (evt) => {
   currentTimeLabel.text = `${hours}:${mins}`;
   //heartRateLabel.text = "93"
 }
+if (BodyPresenceSensor) {
+  const body = new BodyPresenceSensor();
+  body.addEventListener("reading", () => {
+    if (!body.present) {
+      hrm.stop();
+      heartRateLabel.text = "--"
+    } else {
+      hrm.start();
+      heartRateLabel.text = hrm.heartRate
+    }
+  });
+  body.start();
+}
 // update the HR
 if (HeartRateSensor && appbit.permissions.granted("access_heart_rate")) {
 //if (HeartRateSensor) {
   //const hrm = new HeartRateSensor({ frequency: 1, batch: 2 });
-  const hrm = new HeartRateSensor({ frequency: 1 });
+  //const hrm = new HeartRateSensor({ frequency: 1 });
   hrm.addEventListener("reading", () => {
     console.log(`Current heart rate: ${hrm.heartRate}`);
     heartRateLabel.text = hrm.heartRate
@@ -51,5 +67,8 @@ if (HeartRateSensor && appbit.permissions.granted("access_heart_rate")) {
     display.on ? hrm.start() : hrm.stop();
   });
 
-  hrm.start();
+  //hrm.start();
+}
+else if (!appbit.permissions.granted("access_heart_rate")) {
+  heartRateLabel.text = "XX"
 }
